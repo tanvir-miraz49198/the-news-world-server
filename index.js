1f134eb8735c80033b7928b4bb3513c91dc9d1a1
tanvir491981 <89649985+tanvir491@users.noreply.github.com>
2023-05-14 12:01:49 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const port = process.env.PORT || 5000;
+
+const categories = require('./data/categories.json')
+
+
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+    res.send('News is running');
+}) 
+
+app.get('/categories' , (req,res) => {
+res.send(categories);
+})
+
+
+app.listen(port, () => {
+    console.log(`News API Is Running on Port : ${port}`)
+});
