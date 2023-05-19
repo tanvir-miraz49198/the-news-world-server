c60194b181c66b0b2fbad5e302cc11a36b6efe0a
tanvir491981 <89649985+tanvir491@users.noreply.github.com>
2023-05-19 23:11:04 +0600

    categeory data send

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const port = process.env.PORT || 5000;
 
 const categories = require('./data/categories.json')
-const categories = require('./data/news.json')
+const news = require('./data/news.json')
 
 
 app.use(cors());
@@ -18,6 +18,32 @@ app.get('/categories' , (req,res) => {
 res.send(categories);
 })
 
+
+
+app.get('/news' , (req,res) => {
+    const id = req.params.id;
+    console.log(id)
+})
+
+app.get('/news/:id' , (req,res) => {
+   const id = req.params.id;
+   console.log(id)
+   const selectedNews = news.find(n => n._id === id)
+   res.send(selectedNews)
+})
+
+
+app.get('/categories/:id' , (req,res) => {
+    const id = /* exeptional way :---  parseInt(req.params.id)*/ req.params.id;
+    if(id == 0 ){
+        res.send(news)
+    }else{
+       
+        const categoryNews = news.filter(n => /* exeptional way :---  parseInt(n.category_id)*/  n.category_id === id);
+        res.send(categoryNews)
+    }
+    
+    })
 
 app.listen(port, () => {
     console.log(`News API Is Running on Port : ${port}`)
