e89f3cf4740c15e77e5d40b54bbfb048d9e14ee8
tanvir491981 <89649985+tanvir491@users.noreply.github.com>
2023-05-19 16:53:00 +0600

    news data add

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const port = process.env.PORT || 5000;
 
 const categories = require('./data/categories.json')
+const categories = require('./data/news.json')
 
 
 app.use(cors());
